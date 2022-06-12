9d879d2e4f01b28516736fa9bbf7bf1d86b7a9ea
Pallab Tewary <pallabtewary243@gmail.com>
2022-06-12 18:56:48 +0530

    Contact page Updated and Header footer optimized

---
+++ js/jsHTMLFiles/navbar.js
@@ -0,0 +1,49 @@
+document.write(`
+<head>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+    <nav class="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
+             <a href="index.html" class="navbar-brand p-0">
+              <h1 class="m-0">Market Shooter</h1>
+             <h6 class="m-0" style = "float: right; margin-right: 100px;">Consistency Matters</h6>
+            <!-- <img src="img/logo.png" alt="Logo"> -->
+         </a>
+         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+             <span class="fa fa-bars"></span>
+        </button>
+         <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav ms-auto py-0" id = "nav">
+
+                <a href="index.html" class="nav-item nav-link">Home</a>
+                <a href="about.html" class="nav-item nav-link">About</a>
+                <a href="courses.html" class="nav-item nav-link">Course</a>
+                 <div class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Social</a>
+                     <div class="dropdown-menu m-0">
+                            <a href="feature.html" class="dropdown-item">Features</a>
+                             <a href="quote.html" class="dropdown-item">Free Quote</a>
+                       <a href="team.html" class="dropdown-item">Our Team</a>
+                             <a href="testimonial.html" class="dropdown-item">Testimonial</a>
+                           <a href="404.html" class="dropdown-item">404 Page</a>
+                        </div>
+                 </div>
+                    <a href="contact.html" class="nav-item nav-link" id="sup">Support</a>
+                </div>
+                <a href="" class="btn btn-light rounded-pill text-dark py-2 px-4 ms-lg-5">Trade Gallery</a>
+           </div>
+        </nav>
+        
+        <script type="text/javascript">
+            const currentLocation = location.href;
+            const menuItem = document.querySelectorAll('a');
+            const menuLength = menuItem.length
+            for(let i = 0; i < menuLength; i++){
+                if(menuItem[i].href === currentLocation){
+                    menuItem[i].className += " active"
+                }
+            }
+        
+        </script>
+
+`);
